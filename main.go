9a63bbeb14deeebb6cedacaf4d080759405a7da5
{{ .user }} <{{ .email }}>
2021-02-11 10:56:02 +0000

    fix: tried to create an existing variable

--- main.go
+++ main.go
@@ -42,8 +42,8 @@ func main() {
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
-		_, err := fmt.Scanln(&shouldConvertAgain)
-		if err != nill {
+		_, err = fmt.Scanln(&shouldConvertAgain)
+		if err != nil {
 			printError(errReadingInput)
 		}
 
